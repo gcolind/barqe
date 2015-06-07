7035e51bb3d56951c3233ef36c3226473796f3c4
gcolind <gcolin@pdx.edu>
2015-06-07 16:37:26 -0700

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var app = angular
+  .module('Barqe', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'firebase',
+    'toaster',
+    'angularMoment'
+  ])
+  .constant('FURL', 'https://barqe.firebaseio.com/')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/browse.html',
+        controller: 'BrowseController'
+      })
+      .when('/browse/:taskId', {
+        templateUrl: 'views/browse.html',
+        controller: 'BrowseController'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthController'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
